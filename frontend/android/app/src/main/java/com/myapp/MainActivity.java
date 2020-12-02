a5144edb1f4ee99d9aa03b2faa92a43b7889808b
harel350 <harel2351@gmail.com>
2020-12-02 22:13:54 +0200

    Update MainActivity.java

--- frontend/android/app/src/main/java/com/myapp/MainActivity.java
+++ frontend/android/app/src/main/java/com/myapp/MainActivity.java
@@ -2,8 +2,7 @@ package com.myapp;
 
 import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
-  I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
-  sharedI18nUtilInstance.allowRTL(context, true);
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
